a84f01de79e7ec6b9110b63c1f69fe504a359feb
Jon Searle <jonsearle@gmail.com>
2025-11-19 15:18:19 +0000

    Add Our Services section with expand/collapse functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ReactiveHeader from "@/components/ReactiveHeader";
 import ReviewsSection from "@/components/ReviewsSection";
+import ServicesSection from "@/components/ServicesSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <ReactiveHeader />
       <ReviewsSection />
+      <ServicesSection />
     </main>
   );
 }
